3da02dc0f8d195c941dc7767cda5acbb6de96be3
liufeng <164806387@qq.com>
2024-01-29 15:57:37 +0800

    Add files via upload

--- functions/_worker.js
+++ functions/_worker.js
@@ -3,13 +3,7 @@ export default {
     let PROXY_HOST = env.PROXY_HOST
     if (PROXY_HOST) {
       const url = new URL(request.url)
-      const response = fetch(PROXY_HOST + url.pathname + url.search, {
-        method: request.method,
-        headers: request.headers,
-        body: request.body,
-        redirect: 'follow'
-      })
-      return response
+      return fetch(PROXY_HOST + url.pathname + url.search, request)
     } else {
       return new Response('PROXY_HOST的环境变量未设置！', { status: 500 })
     }
